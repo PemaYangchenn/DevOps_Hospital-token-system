e0ad79affd19f3fb807dc86ac6ced1fb115919ea
Asseh <upadhaya.asseh@gmail.com>
2023-10-20 19:20:31 +0000

    created _model folder and userModels.js file inside it

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'})
+const app = require('./app')
+
+
+const DB = process.env.DATABASE.replace('PASSWORD', process.env.DATABASE_PASSWORD,)
+mongoose.connect(DB).then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+}).catch(error  => console.log(error));
+
+// starting the port server on port 4001
+const port = 4001
+app.listen(port, () => {
+    console.log(`app is running on port ${port} ... `);
+})
